72a6f69f4ae35d1557b012cce0317ea2440f8a74
sumitkumards07 <sumitkumards07@gmail.com>
2026-03-22 03:05:41 +0530

    fix(auth): totally isolate login page from massive CMS widget Javascript bundle to permanently prevent unexpected dashboard plugins from throwing background exceptions and severing the secure form submission loops

--- cms/functions/HTML/login-js.php
+++ cms/functions/HTML/login-js.php
@@ -2,7 +2,7 @@
 <?php echo "<script src='$AaliLINK/assets/js/scripts.js'></script>"; ?>
 <?php echo "<script src='$AaliLINK/assets/js/custom.js'></script>"; ?>
 <?php echo "<script src='$AaliLINK/assets/js/aos.js'></script>"; ?>
-<?php echo "<script src='$AaliLINK/functions/JS/User.js'></script>"; ?>
+<?php echo "<script src='$AaliLINK/functions/JS/User.js?v=3'></script>"; ?>
 
 
 <script>
--- cms/index.php
+++ cms/index.php
@@ -6,14 +6,14 @@
         <meta charset="UTF-8">
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
         <title>Login | <?php echo $company_name ?></title>
-        <?php include($AaliLINK_IN."/functions/HTML/css.php"); ?>
+        <?php include($AaliLINK_IN."/functions/HTML/login-css.php"); ?>
     </head>
     
     <body>
 <div id="app">
             <?php include($AaliLINK_IN."/functions/HTML/login.php"); ?>
         </div>
-        <?php include($AaliLINK_IN."/functions/HTML/js.php"); ?>
+        <?php include($AaliLINK_IN."/functions/HTML/login-js.php"); ?>
     </body>
 </html>
 
