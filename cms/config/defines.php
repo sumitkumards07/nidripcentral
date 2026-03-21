9fcc45169fb67bcf96d06bbf585e4f920ecf9534
sumitkumards07 <sumitkumards07@gmail.com>
2026-03-21 23:41:44 +0530

    fix: make PHP CMS domains dynamic for Docker

--- cms/config/defines.php
+++ cms/config/defines.php
@@ -9,9 +9,11 @@
 	$conn = mysqli_init();
 	mysqli_real_connect($conn, HOST, USER, PASS, DB, 13923, NULL, MYSQLI_CLIENT_SSL);
 	
-	define("DOMAIN","https://nidrip.mraalionline.com/cms");
-	$AaliLINK = "https://nidrip.mraalionline.com/cms";
-	$AaliLINK_IN = $_SERVER['DOCUMENT_ROOT']."/cms";
+	$scheme = isset($_SERVER['HTTP_X_FORWARDED_PROTO']) ? $_SERVER['HTTP_X_FORWARDED_PROTO'] : (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http");
+	$host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : (getenv('RENDER_EXTERNAL_HOSTNAME') ?: 'localhost');
+	define("DOMAIN", $scheme . "://" . $host);
+	$AaliLINK = $scheme . "://" . $host;
+	$AaliLINK_IN = rtrim($_SERVER['DOCUMENT_ROOT'], '/');
 	
 	$gc_query = mysqli_query($conn, "SELECT * FROM `aalierp_contents` ORDER BY company_id DESC LIMIT 1");
 	$row = mysqli_fetch_assoc($gc_query);
